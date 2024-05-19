8e11e2ce5eb69450d6a0f51baf03141591618561
Changyoon-Lee <lckddbsl@gmail.com>
2024-05-19 22:13:56 +0900

    d3 timeline proto

---
+++ src/pages/home/home-page.data.ts
@@ -0,0 +1,42 @@
+export const data = {
+  id1: {
+    times: [
+      {
+        start: new Date("2022-02-02T10:00:00"),
+        end: new Date("2022-02-03T01:00:00"),
+      },
+      {
+        start: new Date("2022-02-06T09:10:00"),
+        end: new Date("2022-02-06T09:20:00"),
+      },
+      {
+        start: new Date("2022-02-07T10:00:00"),
+        end: new Date("2022-02-09T01:00:00"),
+      },
+      {
+        start: new Date("2022-02-10T10:00:00"),
+        end: new Date("2022-02-10T10:05:00"),
+      },
+    ],
+  },
+  id2: {
+    times: [
+      {
+        start: new Date("2022-02-02T10:00:00"),
+        end: new Date("2022-02-03T01:00:00"),
+      },
+      {
+        start: new Date("2022-02-06T09:10:00"),
+        end: new Date("2022-02-06T09:20:00"),
+      },
+      {
+        start: new Date("2022-02-07T10:00:00"),
+        end: new Date("2022-02-09T01:00:00"),
+      },
+      {
+        start: new Date("2022-02-10T10:00:00"),
+        end: new Date("2022-02-10T10:05:00"),
+      },
+    ],
+  },
+};
--- src/pages/layouts/layout.ui.tsx
+++ src/pages/layouts/layout.ui.tsx
@@ -1,12 +1,23 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { NavLink, Outlet } from "react-router-dom";
+import { sessionQueries } from "~entities/session";
 import { pathKeys } from "~shared/lib/react-router";
 
 export function GenericLayout() {
-  const { data } = useSuspenseQuery();
+  const { data } = useSuspenseQuery(sessionQueries.userService.queryOptions());
   return (
     <>
-      <Outlet />
+      <div className="-z-20 fixed inset-0 bg-cover bg-fixed bg-center"
+        style={{ backgroundImage: "url(https://images.unsplash.com/photo-1643228995868-bf698f67d053?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)" }}></div>
+      <div className="fixed inset-0 -z-10 bg-black/70"></div>
+      <div className="sticky z-40 inset-x-0 top-0 flex justify-between max-w-3xl bg-clip-padding bg-white/10 back backdrop-blur-md border-stone-100 ring-1 ring-stone-400 h-12 mx-auto p-2 rounded-b-md font-bold text-stone-50">
+        {data ? <div>유저네비게이션부분</div> : <div>게스트네비게이션</div>}
+      </div>
+
+      <div className="max-w-5xl w-screen mx-auto h-[1000px]">
+        <Outlet />
+        <div className="w-max">o</div>
+      </div>
       <Footer />
     </>
   );
@@ -14,7 +25,7 @@ export function GenericLayout() {
 
 function Footer() {
   return (
-    <footer>
+    <footer className="absolute inset-x-0 text-right bg-stone-50 bg-opacity-90">
       <div className="container">
         <NavLink className="logo-font" to={pathKeys.home()}>
           conduit
